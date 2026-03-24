ca660969a68a3bc80ee48e68b35b13e1a8782de8
pablosingh <pablo22_4@hotmail.com>
2026-03-23 22:49:30 -0300

    Bloque 5

---
+++ 02-tp-integrador-20-3-26/src/bloque05.js
@@ -0,0 +1,12 @@
+const duplicar = (numero) => numero * 2;
+const numeroMas100Por3 = (n) => (n + 100) * 3;
+const mayorA100 = (numero) => numero > 100;
+
+const aplicar = (fn, x) => fn(x);
+
+export const bloque05 = () => {
+  console.log(aplicar(duplicar, 2));
+  console.log(aplicar(numeroMas100Por3, 100));
+  console.log(aplicar(mayorA100, 101));
+  console.log(aplicar(mayorA100, 100));
+};
--- 02-tp-integrador-20-3-26/src/index.js
+++ 02-tp-integrador-20-3-26/src/index.js
@@ -2,10 +2,12 @@ import { bloque01 } from './bloque01.js';
 import { bloque02 } from './bloque02.js';
 import { bloque03 } from './bloque03.js';
 import { bloque04 } from './bloque04.js';
+import { bloque05 } from './bloque05.js';
 
 console.log('TP INTEGRADOR ');
 
 // bloque01();
 // bloque02();
 // bloque03();
-bloque04();
+// bloque04();
+bloque05();
