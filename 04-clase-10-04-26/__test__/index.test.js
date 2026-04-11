549f755ccadbdc038b8d7b71d59276ef6d592bef
pablosingh <pablo22_4@hotmail.com>
2026-04-10 21:56:02 -0300

    Primeros test con Jest

---
+++ 04-clase-10-04-26/__test__/index.test.js
@@ -0,0 +1,5 @@
+const suma = require('../index.js');
+
+test('Suma 2 + 3 : ', () => { 
+    expect(suma(2,3)).toBe(5);
+ });
--- 04-clase-10-04-26/index.js
+++ 04-clase-10-04-26/index.js
@@ -2,3 +2,5 @@ const suma = (a, b) => a + b;
 
 console.log("Suma ");
 console.log(suma(9,1));
+
+module.exports = suma;
